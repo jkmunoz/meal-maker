230c8dd6020d70fa269f781e578d9972377ddf50
unknown <juliannakmunoz@gmail.com>
2023-12-20 12:54:21 -0600

    added all courses for functional testing.

--- meal-maker.js
+++ meal-maker.js
@@ -4,23 +4,46 @@ let menu = {
         mains: [],
         desserts: []
     },   
-    addDishToCourse(courseName, dishName, dishPrice) { 
-        let dish = {
-        name: this.dishName,
-        price: this.dishPrice
-       }
-       for (course in courseName)
-       dish.push(course);
-    }, 
-    getRandomDishFromCourse(courseName) {
-        let dishes = {
-            appetizers: this.appetizers,
-            mains: this.mains,
-            desserts: this.desserts
-        }
-        return dishes;
-    }
 
+    // Add a dish to the array of the chosen course. 
+    addDishToCourse(courseName, dishName, dishPrice) { 
+        const dish = {
+        name: dishName,
+        price: dishPrice
+       };
+       this.courses[courseName].push(dish);
+    }, 
+
+    // Get a random dish from the course on the menu.
+    getRandomDishFromCourse(courseName) {
+        const dishes = this.courses[courseName];
+        const randomIndex = Math.floor(Math.random() * dishes.length);
+        return dishes[randomIndex];
+    },
+
+    generateRandomMeal() {
+        let appetizer = this.getRandomDishFromCourse('appetizers');
+        let main = this.getRandomDishFromCourse('mains');
+        let dessert = this.getRandomDishFromCourse('desserts');
+
+        let totalPrice = appetizer.price + main.price + dessert.price;
+
+        return `Your chosen menu is ${appetizer.name}, ${main.name}, and ${dessert.name}. Your total is $${totalPrice}.`;
+    }
 };
 
-console.log(menu.addDishToCourse(Dinner, spaghetti, 18))
+// Appetizers
+menu.addDishToCourse('appetizers', 'mozzarella sticks', 7);
+menu.addDishToCourse('appetizers', 'fried calamari', 12);
+menu.addDishToCourse('appetizers', 'bread sticks', 0);
+// Mains
+menu.addDishToCourse('mains', 'Gnocci', 17);
+menu.addDishToCourse('mains', 'Pasta Primavera', 20);
+menu.addDishToCourse('mains', 'Pesto Pasta', 15);
+//Desserts
+menu.addDishToCourse('desserts', 'Cheese cake', 12);
+menu.addDishToCourse('desserts', 'Canole cream', 7);
+menu.addDishToCourse('desserts', 'Italian cream cake', 9);
+
+let meal = menu.generateRandomMeal();
+console.log(meal);
